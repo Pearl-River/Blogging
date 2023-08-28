11750e7895af037b4581e58219ebdbc0572551c6
Pearl-River <nguyenngocha4091@gmail.com>
2023-08-29 00:44:33 +0700

    Completed SignUpPage

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import Label from "../components/label/Label";
@@ -7,15 +7,21 @@ import Field from "../components/field/Field";
 import IconEyeClose from "../components/icon/IconEyeClose";
 import IconEyeOpen from "../components/icon/IconEyeOpen";
 import Button from "../components/button/Button";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db } from "../firebase/firebase-config";
+import { useNavigate } from "react-router-dom";
+import { addDoc, collection } from "firebase/firestore";
+import { values } from "lodash";
 
 const SignUpPageStyles = styled.div`
   min-height: 100vh;
   padding: 40px;
-
   .logo {
     margin: 0 auto 20px;
   }
-
   .heading {
     text-align: center;
     color: ${(props) => props.theme.primary};
@@ -23,14 +29,26 @@ const SignUpPageStyles = styled.div`
     font-size: 40px;
     margin-bottom: 60px;
   }
-
   .form {
     max-width: 600px;
     margin: 0 auto;
   }
 `;
 
+const schema = yup.object({
+  fullname: yup.string().required("Please enter your fullname"),
+  email: yup
+    .string()
+    .email("Please enter valid email address")
+    .required("Please enter valid email address"),
+  password: yup
+    .string()
+    .min(8, "Your password must be at least 8 characters or greater")
+    .required("Please enter your password"),
+});
+
 const SignUpPage = () => {
+  const navigate = useNavigate();
   const {
     control,
     handleSubmit,
@@ -39,16 +57,38 @@ const SignUpPage = () => {
     reset,
   } = useForm({
     mode: "onChange",
+    resolver: yupResolver(schema),
   });
-  const handleSignUp = (values) => {
+  const handleSignUp = async (values) => {
     if (!isValid) return;
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 5000);
+    console.log(values);
+    const user = await createUserWithEmailAndPassword(
+      auth,
+      values.email,
+      values.password
+    );
+    await updateProfile(auth.currentUser, {
+      displayName: values.fullname,
     });
+    const colRef = collection(db, "user");
+    await addDoc(colRef, {
+      fullname: values.fullname,
+      email: values.email,
+      password: values.password,
+    });
+    toast.success("Register successfully!!!");
+    navigate("/");
   };
   const [togglePassword, setTogglePassword] = useState(false);
+  useEffect(() => {
+    const arrErrores = Object.values(errors);
+    if (arrErrores.length > 0) {
+      toast.error(arrErrores[0]?.message, {
+        pauseOnHover: false,
+        delay: 0,
+      });
+    }
+  }, [errors]);
   return (
     <SignUpPageStyles>
       <div className="container">
